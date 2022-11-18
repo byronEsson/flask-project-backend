2b3ff52d10cf005b812adc6f0fb44fdf28511bad
byronEsson <byronesson767@gmail.com>
2022-11-18 10:34:20 +0000

    build app

---
+++ my_flask_api/flask/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, abort
+from model import findKeyWords
+import json
+app = Flask('my_favourite_api') 
+
+
+@app.route('/model', methods=['POST'])
+def model():
+    try:
+        words = request.json  
+        return json.dumps(findKeyWords(words['positive'], words['negative'] ))
+    except:
+        abort(400)
+
+@app.route("/*")
+def not_found():
+    abort(404)
+
+@app.errorhandler(400)
+def handle_400(e):
+    return 'Request object not formatted correctly', 400
